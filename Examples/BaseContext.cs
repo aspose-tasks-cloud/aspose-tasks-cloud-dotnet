1b7e56fce38c2e42bd71f468128d9c8687dc11ac
Mateen Sajjad <mateen.sajjad@aspose.com>
2019-08-07 02:17:28 +0500

    Removed redundant credentials

--- Examples/BaseContext.cs
+++ Examples/BaseContext.cs
@@ -5,8 +5,8 @@ namespace aspose.tasks.csharp
 {
     public class BaseContext
     {
-        public static string AppKey = "b125f13bf6b76ed81ee990142d841195";
-        public static string AppSid = "78946fb4-3bd4-4d3e-b309-f9e2ff9ac6f9"   ; 
+        public static string AppKey = "";
+        public static string AppSid = ""   ; 
         public  string DataFolder     ="";
     }
 }
