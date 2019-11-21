67647ea28def1388ee0ff8488d6d9f5dc6d0fb67
IvanAndreychikov <ivan.andreychikov@gmail.com>
2019-11-21 10:58:09 +0500

    [N/A] - Removed redundant data.

--- Examples/BaseContext.cs
+++ Examples/BaseContext.cs
@@ -5,8 +5,8 @@ namespace aspose.tasks.csharp
 {
     public class BaseContext
     {
-        public static string AppKey = "3cbe143d4df5ac2170602f52bd708053";
-        public static string AppSid = "78946fb4-3bd4-4d3e-b309-f9e2ff9ac6f9"   ; 
+        public static string AppKey = "";
+        public static string AppSid = ""; 
         public  string DataFolder     ="";
     }
 }
